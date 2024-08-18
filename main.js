ad1a945c90b1a027ad94a725ffeb3ff9b8afde30
Jonathan Martinez Gutierrez <utm180149ti@ut-morelia.edu.mx>
2024-08-17 19:12:38 -0600

    1

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
 import * as THREE from 'three';
 
 const scene = new THREE.Scene();
+
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.listen(port, '0.0.0.0', () => {
+  console.log(`App listening on port ${port}`);
+});
